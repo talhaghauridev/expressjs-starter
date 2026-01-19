991d490f8cb49ed069fa438cee9265993bdf4024
Talha Ghauri <talhaghauridev@gmail.com>
2026-01-19 21:49:40 +0500

    add admin route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import authRoutes from './auth.route';
 import usersRoutes from './users.route';
+import adminRoutes from './admin.route';
+
 const routes = express.Router();
 
 routes.use('/auth', authRoutes);
 routes.use('/users', usersRoutes);
+routes.use('/admin', adminRoutes);
 
 export default routes;
