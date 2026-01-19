862d13d56cf36b9f60f526d5a89aac109883a0c9
Talha Ghauri <talhaghauridev@gmail.com>
2026-01-19 19:11:58 +0500

    fix

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -5,8 +5,7 @@ import { UserRepository } from '@/repositories/users.repository';
 import ApiError from '@/utils/api-error';
 import { comparePassword, hashPassword } from '@/utils/password';
 import { StorageService } from './storage.service';
-import { getUpdatedFields } from '@/utils/object-utils';
-
+import { getUpdatedFields } from '@/utils/helpers';
 export class UsersService {
   static async getProfile(userId: string) {
     const user = await UserRepository.findById(userId, { password: false });
